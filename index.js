d722442e290b59caf35310b2404b07fefe81bff9
gtpWebDev <33732289+gtpWebDev@users.noreply.github.com>
2024-02-23 09:51:15 +0000

    Add admin dashboard as page template
    Template from previous project used for page styling,
    simplifying it to a header, a side bar, and a content area for
    the book cards.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("HELLO")
+
+
+const myLibrary = [];
+
+//let book1 = new Book("Life of Pi", "Yann Martel", 300, true)
+//console.log("Book1, a Book object, with object properties and methods:",book1)
+//console.log("Book info object method invocation:", book1.info())
+
+addBookToLibrary("Life of Pi", "Yann Martel", 300, true)
+addBookToLibrary("The Bible", "Anon", 2300, false)
+console.table(myLibrary)
+
+function Book(name, author, pages, readOrNot) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.readOrNot = readOrNot
+  this.info = function() {
+    if (readOrNot) {
+      return `${name} by ${author}, ${pages} pages, previously read`
+    } else {
+      return `${name} by ${author}, ${pages} pages, not read yet`
+    }
+  }
+}
+
+function addBookToLibrary(name, author, pages, readOrNot) {
+
+  let newBook = new Book(name, author, pages, readOrNot)
+  myLibrary.push(newBook)
+
+}
+
