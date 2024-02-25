c30805d8f3ca2f62ec68150b3c4572dfa321e956
gtpWebDev <33732289+gtpWebDev@users.noreply.github.com>
2024-02-25 12:51:06 +0000

    Improve main page and dialog formatting
    Alignment of elements in dialog form improved with multiple grids.
    Overall page simplified with header and main body, and page
    width restricted to maximum of 1500px and 80vw.

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 
-// live code on page load
+/*  Potential improvements:
+    - prevent default on submit button in dialog prevents form validation - add back end and remove prevent default
+    - add book form styling could be improved
+*/
 
 const myLibrary = [];
 
-addBookToLibrary(
-  "Life of Pi",
-  "Yann Martel",
-  300,
-  "A boy is travelling from India to Canada when the ship sinks, leaving him in a small lifeboat with only an orangutan, a hyena, and an adult bengali tiger...",
-  true
-)
-addBookToLibrary(
-  "David Copperfield",
-  "Charles Dickens",
-  2300,
-  "The story of a young man's adventures on his journey from an unhappy and impoverished childhood to the discovery of his vocation as a successful novelist.",
-  false
-)
-addBookToLibrary(
-  "Lord of the Rings",
-  "J R R Tolkein",
-  1200,
-  " A hobbit named Frodo inherits the One Ring, which can destroy the entire world...",
-  true
-)
-addBookToLibrary(
-  "War and Peace",
-  "Leo Tolstoy",
-  1800,
-  "The main focus is the invasion of Russia by Napoleon in 1812. It follows three well-known characters: Natasha Rostov, Prince Andrei Bolkonsky, and Pierre Bezukhoy.",
-  true
-)
+addBookToLibrary("Life of Pi", "Yann Martel", true, "A boy is travelling from India to Canada when the ship sinks, leaving him in a small lifeboat with only an orangutan, a hyena, and an adult bengali tiger...")
+addBookToLibrary("David Copperfield", "Charles Dickens", false, "The story of a young man's adventures on his journey from an unhappy and impoverished childhood to the discovery of his vocation as a successful novelist.")
+addBookToLibrary("Lord of the Rings", "J R R Tolkein", false, " A hobbit named Frodo inherits the One Ring, which can destroy the entire world...")
+addBookToLibrary("War and Peace", "Leo Tolstoy", true, "The main focus is the invasion of Russia by Napoleon in 1812. It follows three well-known characters: Natasha Rostov, Prince Andrei Bolkonsky, and Pierre Bezukhoy.")
 
 displayBooksWithMap()
 
@@ -43,8 +22,7 @@ let addBookButton = document.querySelector("#open-add-book-dialog")
 addBookButton.addEventListener("click", () => {
   
   addBookDialog.showModal()
-  // the modal usually has display: none, which ensures it is not shown.
-  // Applying the id applies a grid display which makes it visible, so this can only be done when showModal is applied
+  // The css for this id removes display: none, so styling has to be applied after showModal
   addBookDialog.setAttribute("id","add-book-dialog")
 })
 
@@ -65,7 +43,6 @@ addBookSubmit.addEventListener("click", (event) => {
   addBookToLibrary(
     document.querySelector("#form-book-name").value,
     document.querySelector("#form-book-author").value,
-    document.querySelector("#form-book-pages").value,
     document.querySelector("#form-book-synopsis").value,
     document.querySelector("#form-book-read-or-not").value,
   )
@@ -77,32 +54,24 @@ addBookSubmit.addEventListener("click", (event) => {
 
 
 
-// Book object constructor function
-function Book(name, author, pages, synopsis, readOrNot) {
-
+// Book object constructor
+function Book(name, author, readOrNot, synopsis) {
   this.name = name;
   this.author = author;
-  this.pages = pages;
   this.readOrNot = readOrNot
   this.synopsis = synopsis
-  this.info = function() {
-    if (readOrNot) {
-      return `${name} by ${author}, ${pages} pages, previously read`
-    } else {
-      return `${name} by ${author}, ${pages} pages, not read yet`
-    }
-  }
-
 }
 
-function addBookToLibrary(name, author, pages, synopsis, readOrNot) {
-  let newBook = new Book(name, author, pages, synopsis, readOrNot)
-  myLibrary.push(newBook)
+
+function addBookToLibrary(name, author, readOrNot, synopsis) {
+  myLibrary.push(new Book(name, author, readOrNot, synopsis))
 }
+
 
 function removeBook(index) {
   myLibrary.splice(index,1)
 }
+
 
 function toggleReadOrNot(index) {
   myLibrary[index].readOrNot = !myLibrary[index].readOrNot
@@ -132,16 +101,17 @@ function displayBooksWithMap() {
         
   }).join('')
 
-  // Note innerHTML generates a complete rebuild of the parent
   libraryBookContainer.innerHTML = createdDOM
 
   // associate button functionality to the created DOM elements
   myLibrary.forEach((elem, index) => {
+
     let removeBookButton = document.querySelector(`#remove-book-${index}`)
     removeBookButton.addEventListener("click",() => {
       removeBook(index)
       displayBooksWithMap()
     })
+    
     let toggelReadOrNotButton = document.querySelector(`#toggle-read-book-${index}`)
     toggelReadOrNotButton.addEventListener("click",() => {
       toggleReadOrNot(index)
@@ -150,65 +120,3 @@ function displayBooksWithMap() {
   })
 
 }
-
-
-// A second approach to displaying the library books, not used
-
-/*
-
-let loadWithDOM = document.querySelector("#load-with-dom")
-loadWithDOM.addEventListener("click",displayBooksAmendDOM)
-
-function displayBooksAmendDOM() {
-
-  let libraryBookContainer = document.querySelector("#library-book-container")
-  libraryBookContainer.innerHTML = "" // clear out the content first
-
-  myLibrary.forEach((elem) => {
-    
-    let indivBookContainer = document.createElement("div")
-    indivBookContainer.setAttribute("class","indiv-book-container")
-
-    let backgroundColor = elem.readOrNot ? "#ccffcc" : "#ff9980"
-    indivBookContainer.setAttribute("style",`background-color: ${backgroundColor}`)
-
-    // title and author
-    let bookTitleAndAuthor = document.createElement("div")
-    bookTitleAndAuthor.setAttribute("class","book-title-and-author")
-    let bookTitle = document.createElement("p")
-    bookTitle.setAttribute("class","book-title")
-    bookTitle.textContent = elem.name
-    let bookAuthor = document.createElement("p")
-    bookAuthor.setAttribute("class","book-author")
-    bookAuthor.textContent = `by ${elem.author}`
-    bookTitleAndAuthor.appendChild(bookTitle)
-    bookTitleAndAuthor.appendChild(bookAuthor)
-    indivBookContainer.appendChild(bookTitleAndAuthor)
-    // title and author
-    let bookSynopsis = document.createElement("p")
-    bookSynopsis.setAttribute("class","book-synopsis")
-    bookSynopsis.textContent = elem.synopsis
-    indivBookContainer.appendChild(bookSynopsis)
-    // book options
-    let bookOptions = document.createElement("div")
-    bookOptions.setAttribute("class","book-options")
-    let toggleReadOrNotIcon = document.createElement("img")
-    toggleReadOrNotIcon.setAttribute("class","icon")
-    let readOrNotImgUrl = elem.readOrNot ? "/img/eye-minus.svg" : "/img/eye-plus.svg"
-    toggleReadOrNotIcon.setAttribute("src",readOrNotImgUrl)
-    let removeBookIcon = document.createElement("img")
-    removeBookIcon.setAttribute("class","icon")
-    removeBookIcon.setAttribute("src","/img/library-remove.svg")
-    bookOptions.appendChild(toggleReadOrNotIcon)
-    bookOptions.appendChild(removeBookIcon)
-    indivBookContainer.appendChild(bookOptions)
-
-    // finally append created structure to parent element
-    
-    libraryBookContainer.appendChild(indivBookContainer)
-  })
-
-}
-
-*/
-
