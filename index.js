b33493eb2e20f4cfeaee76c8117cd72fba04966e
gtpWebDev <33732289+gtpWebDev@users.noreply.github.com>
2024-02-25 07:34:29 +0000

    Improve display books and begin add books
    Replaced display books function with a tidier .map approach.
    Added an "add book" capability which opens a modal dialog
    and a form in the dialog to add a new book. It's functional
    but requires better styling.

--- index.js
+++ index.js
@@ -42,12 +42,119 @@ addBookToLibrary(
   4. Toggle status to readOrNot = true.
 */
 
-displayBooksAmendDOM()
 
-// Option 1
+// add webpage button functionality
+let loadWithMap = document.querySelector("#load-with-map")
+loadWithMap.addEventListener("click",displayBooksWithMap)
+
+
+
+// Book object constructor function
+function Book(name, author, pages, synopsis, readOrNot) {
+
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.readOrNot = readOrNot
+  this.synopsis = synopsis
+  this.info = function() {
+    if (readOrNot) {
+      return `${name} by ${author}, ${pages} pages, previously read`
+    } else {
+      return `${name} by ${author}, ${pages} pages, not read yet`
+    }
+  }
+
+}
+
+
+
+// Add book functionality - event listeners to open up the dialog, submit and cancel
+
+/* Two important notes about modal:
+  1. preventDefault prevents the usual behaviour of posting the collected information to a server (here it is being added to an array only).
+  2. the modal usually has display: none. styling the modal implicitly makes it visible, so need to add the styling on showModal() and remove it on close()
+*/
+const addBookDialog = document.querySelector("dialog")
+let addBookButton = document.querySelector("#open-add-book-dialog")
+
+addBookButton.addEventListener("click", () => {
+  
+  addBookDialog.showModal()
+  addBookDialog.setAttribute("id","add-book-dialog")
+})
+
+let addBookCancel = document.querySelector("#add-book-cancel")
+addBookCancel.addEventListener("click", (event) => {
+  // prevent usual behaviour of form submit to send information to a server
+  event.preventDefault();
+  addBookDialog.setAttribute("id","add-book-dialog-no-display")
+  addBookDialog.close();
+  addBookButton.focus();
+})
+
+let addBookSubmit = document.querySelector("#add-book-submit")
+addBookSubmit.addEventListener("click", (event) => {
+  // prevent usual behaviour of form submit to send information to a server
+  event.preventDefault();
+  addBookToLibrary(
+    document.querySelector("#form-book-name").value,
+    document.querySelector("#form-book-author").value,
+    document.querySelector("#form-book-pages").value,
+    document.querySelector("#form-book-synopsis").value,
+    document.querySelector("#form-book-read-or-not").value,
+  )
+  addBookDialog.removeAttribute("id","add-book-dialog")
+  addBookDialog.close();
+  addBookButton.focus();
+})
+
+
+function addBookToLibrary(name, author, pages, synopsis, readOrNot) {
+  let newBook = new Book(name, author, pages, synopsis, readOrNot)
+  myLibrary.push(newBook)
+}
+
+
+
+// Option 1 - most elegant using map, join and adding it using innerHTML
+function displayBooksWithMap() {
+
+  let libraryBookContainer = document.querySelector("#library-book-container")
+
+  let createdDOM = myLibrary.map((elem) => {
+
+    return `
+      <div style="background-color: ${elem.readOrNot ? "#ccffcc" : "#ff9980"}" class="indiv-book-container">
+        <div class="book-title-and-author">
+          <p class="book-title">${elem.name}</p>
+          <p class="book-author">by ${elem.author}</p>
+        </div>
+        <p class="book-synopsis">${elem.synopsis}</p>
+        <div class="book-options">
+          <img class="icon" src="${elem.readOrNot ? "/img/eye-minus.svg" : "/img/eye-plus.svg"}">
+          <img class="icon" src="img/library-remove.svg">
+        </div>
+      </div>
+    `
+        
+  }).join('')
+
+  // Note innerHTML generates a coplete rebuild of the parent - perfect here
+  libraryBookContainer.innerHTML = createdDOM
+
+}
+
+
+// Option 2 for displaying books (keep in backgorund)
+
+//let loadWithDOM = document.querySelector("#load-with-dom")
+//loadWithDOM.addEventListener("click",displayBooksAmendDOM)
+
 function displayBooksAmendDOM() {
 
   let libraryBookContainer = document.querySelector("#library-book-container")
+  libraryBookContainer.innerHTML = "" // clear out the content first
 
   myLibrary.forEach((elem) => {
     
@@ -87,36 +194,11 @@ function displayBooksAmendDOM() {
     bookOptions.appendChild(toggleReadOrNotIcon)
     bookOptions.appendChild(removeBookIcon)
     indivBookContainer.appendChild(bookOptions)
+
     // finally append created structure to parent element
+    
     libraryBookContainer.appendChild(indivBookContainer)
   })
 
 }
-
-
-
-// Book object constructor function
-function Book(name, author, pages, synopsis, readOrNot) {
-
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.readOrNot = readOrNot
-  this.synopsis = synopsis
-  this.info = function() {
-    if (readOrNot) {
-      return `${name} by ${author}, ${pages} pages, previously read`
-    } else {
-      return `${name} by ${author}, ${pages} pages, not read yet`
-    }
-  }
-
-}
-
-
-function addBookToLibrary(name, author, pages, synopsis, readOrNot) {
-  let newBook = new Book(name, author, pages, synopsis, readOrNot)
-  myLibrary.push(newBook)
-}
-
 
